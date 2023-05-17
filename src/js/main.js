077c81faacf2e74674539850badc6986a4e9fe16
thomasrouiller <thomas.rouiller@studentfr.ch>
2023-05-17 14:18:51 +0200

    continuing js

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
 var button = document.querySelector(".menu-close");
 var menu = document.querySelector(".menu-open");
+var logo = document.querySelector(".menu-container");
+var logo2 = document.querySelector(".menu-small-logo");
 var links = document.querySelectorAll(".menu-link");
 
 var openMenu = function () {
   menu.classList.toggle("is-open");
+  logo.classList.toggle("is-open");
+  logo2.classList.toggle("is-open");
   button.classList.toggle("is-active");
 };
 
