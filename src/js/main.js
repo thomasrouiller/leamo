b6a7d48c921c3d31431839d62392449e6e77d80a
thomasrouiller <thomas.rouiller@studentfr.ch>
2023-05-22 15:05:49 +0200

    added the fonts

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,20 @@
 var button = document.querySelector(".menu-close");
 var menu = document.querySelector(".menu-open");
 var logo = document.querySelector(".menu-container");
-var logo2 = document.querySelector(".menu-small-logo");
+var logoNav = document.querySelector(".menu-small-logo");
 var links = document.querySelectorAll(".menu-link");
 var navigation = document.querySelector(".menu-credits-informations");
-var button2 = document.querySelector(".menu-lange-close");
-var menu2 = document.querySelector(".mmenu-lang-open");
+var buttonLanguage = document.querySelector(".menu-lang-close");
+var menuLanguage = document.querySelector(".menu-lang-open");
 
 var openMenu = function () {
   menu.classList.toggle("is-open");
   logo.classList.toggle("is-open");
-  logo2.classList.toggle("is-open");
+  logoNav.classList.toggle("is-open");
   button.classList.toggle("is-active");
   navigation.classList.toggle("is-open");
-  menu2.classList.toggle("is-open");
-  button2.classList.toggle("is-active");
+  menuLanguage.classList.toggle("is-open");
+  buttonLanguage.classList.toggle("is-active");
 };
 
 button.addEventListener("click", openMenu);
