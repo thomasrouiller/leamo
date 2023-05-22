5031dfbbaaf8a025cf649c172cabe9202af1e6de
thomasrouiller <thomas.rouiller@studentfr.ch>
2023-05-22 13:42:34 +0200

    Revert "still doing js"
    This reverts commit 44dce236a1264b6e93b84c55302b53cbe88a36b3.

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,14 @@
 var button = document.querySelector(".menu-close");
 var menu = document.querySelector(".menu-open");
-var button2 = document.querySelector(".menu-lange-close");
-var menu2 = document.querySelector(".mmenu-lang-open");
 var logo = document.querySelector(".menu-container");
 var logo2 = document.querySelector(".menu-small-logo");
 var links = document.querySelectorAll(".menu-link");
-var navigation = document.querySelector(".menu-credits-informations");
 
 var openMenu = function () {
   menu.classList.toggle("is-open");
-  menu2.classList.toggle("is-open");
   logo.classList.toggle("is-open");
   logo2.classList.toggle("is-open");
-  navigation.classList.toggle("is-open");
   button.classList.toggle("is-active");
-  button2.classList.toggle("is-active");
 };
 
 button.addEventListener("click", openMenu);
