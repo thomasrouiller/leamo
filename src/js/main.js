59077a9675ffd7984a201322b97d319bca5a9b3e
thomasrouiller <thomas.rouiller@studentfr.ch>
2023-05-22 14:08:26 +0200

    putain l'enfer - so many bugs

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,18 @@ var menu = document.querySelector(".menu-open");
 var logo = document.querySelector(".menu-container");
 var logo2 = document.querySelector(".menu-small-logo");
 var links = document.querySelectorAll(".menu-link");
+var navigation = document.querySelector(".menu-credits-informations");
+var button2 = document.querySelector(".menu-lange-close");
+var menu2 = document.querySelector(".mmenu-lang-open");
 
 var openMenu = function () {
   menu.classList.toggle("is-open");
   logo.classList.toggle("is-open");
   logo2.classList.toggle("is-open");
   button.classList.toggle("is-active");
+  navigation.classList.toggle("is-open");
+  menu2.classList.toggle("is-open");
+  button2.classList.toggle("is-active");
 };
 
 button.addEventListener("click", openMenu);
