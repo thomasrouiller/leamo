7cb30d0a2853fdde7439db9fca7d8e248b08aa92
thomasrouiller <thomas.rouiller@studentfr.ch>
2023-05-24 14:09:26 +0200

    polished some stuff, language js still doesn't work, i might give up

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,8 @@ var logo = document.querySelector(".menu-container");
 var logoNav = document.querySelector(".menu-small-logo");
 var links = document.querySelectorAll(".menu-link");
 var navigation = document.querySelector(".menu-credits-informations");
-var buttonLanguage = document.querySelector(".menu-lang-close");
-var menuLanguage = document.querySelector(".menu-lang-open");
+// var buttonLanguage = document.querySelector(".menu-lang-close");
+// var menuLanguage = document.querySelector(".menu-lang-open");
 
 var openMenu = function () {
   menu.classList.toggle("is-open");
@@ -13,11 +13,12 @@ var openMenu = function () {
   logoNav.classList.toggle("is-open");
   button.classList.toggle("is-active");
   navigation.classList.toggle("is-open");
-  menuLanguage.classList.toggle("is-open");
-  buttonLanguage.classList.toggle("is-active");
+  // menuLanguage.classList.toggle("is-open");
+  // buttonLanguage.classList.toggle("is-active");
 };
 
 button.addEventListener("click", openMenu);
+// buttonLanguage.addEventListener("click", openMenu);
 
 links.forEach((link) => {
   link.addEventListener("click", openMenu);
